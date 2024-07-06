ee46fc24b867332fad30467c915e6c253d5ebad1
Adithya Github Work <adithya2011.work@gmail.com>
2024-07-06 07:47:07 +0530

    System command!
    changes:
    1. sysrun:
        echo hello world!
    --Runs a system shell command
    
    2. Bin folder deleted because its a pain to move them individually. Follow instructions in readme file to build the project.

--- src/mainc.cpp
+++ src/mainc.cpp
@@ -9,34 +9,55 @@
 std::string filename;
 std::string toprint;
 std::string function;
+std::string compileChoice;
 std::string opp;
 std::string outfilename = "out.c";
+std::string tosys; // System command
 std::ofstream outfile(outfilename, std::ios_base::app);
 
 void writeStd() // write headers
 {
+    if (!function.empty())
+    {
+        outfile << "void " + function + "(){\n\n\n";
+        outfile << "}";
+    }
+
     outfile << "#include <stdio.h>\n";
     outfile << "#include <stdlib.h>\n";
     outfile << "#include <unistd.h>\n";
     outfile << "#include <string.h>\n\n";
     outfile << "int main(){\n\n\n";
-    
 }
 
-void write(const std::string &filename)
+void write()
 {
-    outfile << "printf(\"" + toprint + "\");" << std::endl;
-    
+    if (!toprint.empty())
+    {
+        outfile << "printf(\"" + toprint + "\");" << std::endl;
+    }
+    if (!tosys.empty())
+    {
+        outfile << "system(\"" + tosys + "\");" << std::endl;
+    }
+    toprint.clear();
+    tosys.clear();
 }
 
 void finalize()
 {
-   outfile << "\n return 0;\n }" << std::endl;
-   outfile.close();
+    outfile << "\n return 0;\n }" << std::endl;
+    outfile.close();
+}
+
+void debug() // macro for debugging
+{
+    std::cout << "test passed" << std::endl;
 }
 
 void c(const std::string &filename)
 {
+
     std::cout << "shllc v1, compiling, please make sure that any out.c file in this directory is deleted to prevent overwriting\n Make sure gcc is present!" << std::endl;
     std::string line;
     std::ifstream file(filename);
@@ -47,7 +68,16 @@ void c(const std::string &filename)
             while (getline(file, line))
             {
                 toprint = line;
-                write(filename);
+                write();
+                break;
+            }
+        }
+        if (line == "sysrun:")
+        {
+            while (getline(file, line))
+            {
+                tosys = line;
+                write();
                 break;
             }
         }
@@ -62,15 +92,25 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     std::string filename = argv[1];
-    
+
     writeStd();
-    c(filename);      // initialize and finalize
+    c(filename); // initialize and finalize
     finalize();
 
-    system("gcc out.c -o shll.exe");
-    system("gcc out.c -o shll.sh");
-    system("gcc out.c -o shll.o");
-    
+    std::cout << "Compile for which operating system? W for windows, L for linux and O for an object file." << std::endl;
+    std::cin >> compileChoice;
+    if (compileChoice == "W")
+    {
+        system("gcc out.c -o shll.exe");
+    }
+    if (compileChoice == "L")
+    {
+        system("gcc out.c -o shll.sh");
+    }
+    if (compileChoice == "O")
+    {
+        system("gcc out.c -o shll.o");
+    }
 
     return 0;
 }
---
+++ src/mainj.cpp
@@ -0,0 +1,7 @@
+#include "iostream"
+
+int main()
+{
+    std::cout << "Java support in the works!\n" << std::endl;
+    return 0;
+}
